a6f68348e30f901517c58257159f22e8d8363a26
Emil Sharier <emil.sharier.dev@gmail.com>
2026-01-22 11:13:52 +0530

    feat: add migration for new database schema including enums and tables for pages and blocks; update generated schema file

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260122_054335 from './20260122_054335';
+
+export const migrations = [
+  {
+    up: migration_20260122_054335.up,
+    down: migration_20260122_054335.down,
+    name: '20260122_054335'
+  },
+];
